8d39f3adba77a5bb0c2117910685de659e53e1ad
gbbenson68 <gbbenson68@gmail.com>
2019-06-11 10:41:23 -0400

    Move summary calculation to calc.js
    Moved summary calculation to calc.js.

--- assets/scripts/calculator/ui.js
+++ assets/scripts/calculator/ui.js
@@ -1,6 +1,7 @@
 'use strict'
 const pkgName = 'calc.ui'
 
+const calc = require('./calc')
 const config = require('../config')
 const store = require('../store')
 const util = require('../util')
@@ -13,40 +14,13 @@ const listReadingsTemplate = require('../templates/readings_listing.handlebars')
 **       loop over the data twice to determine max and min.
 */
 const renderSummary = (readings) => {
-  let maxOdo = 0
-  let fuelSum = 0
-  let priceSum = 0.0
-  readings.forEach((reading) => {
-    fuelSum = fuelSum + reading.fuel_amount // The linter complains if I use +=
-    priceSum = priceSum + reading.price // The linter complains if I use +=
-    if (reading.odometer_reading > maxOdo) {
-      maxOdo = reading.odometer_reading
-    }
-  })
-
-  let minOdo = maxOdo // To make sure we get minimum.
-  readings.forEach((reading) => {
-    if (reading.odometer_reading < minOdo) {
-      minOdo = reading.odometer_reading
-    }
-  })
-  const milesDriven = maxOdo - minOdo // This should always be an integer.
-  let milesPerGallon = 0
-  let pricePerGallon = 0
-  if (fuelSum !== 0) {
-    milesPerGallon = (milesDriven / fuelSum).toFixed(1)
-    pricePerGallon = (priceSum / fuelSum).toFixed(2)
-  } else {
-    milesPerGallon = 0.0
-    pricePerGallon = 0.00
-  }
-
+  const summaryObj = calc.getSummaryInfo(readings)
   let textToRender = '<div class=\'sum-wrapper\'>'
   textToRender += `<div class='summary-detail'>Summary:</div>`
-  textToRender += `<div class='summary-detail'>Distance driven: ${milesDriven} mi</div>`
-  textToRender += `<div class='summary-detail'>Total Fuel: ${fuelSum} gal</div>`
-  textToRender += `<div class='summary-detail'>MPG: ${milesPerGallon}</div>`
-  textToRender += `<div class='summary-detail'>PPG: ${pricePerGallon}</div>`
+  textToRender += `<div class='summary-detail'>Distance driven: ${summaryObj.milesDriven} mi</div>`
+  textToRender += `<div class='summary-detail'>Total Fuel: ${summaryObj.fuelSum} gal</div>`
+  textToRender += `<div class='summary-detail'>MPG: ${summaryObj.milesPerGallon}</div>`
+  textToRender += `<div class='summary-detail'>PPG: ${summaryObj.pricePerGallon}</div>`
   textToRender += '</div>'
   $(config.summaryId).html(textToRender)
 }
@@ -61,7 +35,7 @@ const onIndexSuccessSilent = responseData => {
 }
 
 const onIndexFailureSilent = responseData => {
-  util.displaySuccessFail(`${pkgName}.onIndexSuccessSilent()`, 'Whoa... Couldn\'t get summary data. Please try the \'Load Entries\' button.', true, responseData)
+  util.displaySuccessFail(`${pkgName}.onIndexSuccessSilent()`, 'Whoa... Couldn\'t get summary data. Please try the \'Load Entries\' button.', false, responseData)
   util.timeoutMessage()
 }
 
