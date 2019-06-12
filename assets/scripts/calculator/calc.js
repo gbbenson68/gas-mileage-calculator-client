2aa517a64aa63c326d917122ca0313f260f5057f
gbbenson68 <gbbenson68@gmail.com>
2019-06-12 17:42:21 -0400

    FINALLY got last bit check working
    FINALLY - the paramsAreValid() function now accurately checks if the
    entered odo reading is greater than the next greatest odo reading.

--- assets/scripts/calculator/calc.js
+++ assets/scripts/calculator/calc.js
@@ -27,12 +27,14 @@ const todayDate = () => {
 ** paramsAreValid()
 */
 const paramsAreValid = (formData) => {
-  const newTransDate = formData.reading.transaction_date
-  const newOdoReading = formData.reading.odometer_reading
+  const newEntryTransDate = formData.reading.transaction_date
+  const newEntryOdoReading = formData.reading.odometer_reading
+  const lastTransDate = store.allReadings[store.allReadings.length - 1].transaction_date
+  const lastOdoReading = store.allReadings[store.allReadings.length - 1].odometer_reading
   const today = todayDate()
 
   // Kick out an error if the new entry date is later than today.
-  if (newTransDate > today) {
+  if (newEntryTransDate > today) {
     util.displaySuccessFail(`${pkgName}.paramsAreValid`, 'You cannot enter a date later than today, please try again!', false, '')
     util.timeoutMessage()
     util.resetForm()
@@ -40,22 +42,47 @@ const paramsAreValid = (formData) => {
   }
 
   // Find the most recent transaction before the to-be-added entry and perform some data checks.
-  let lastTransDate = ''
-  let lastOdoReading = 0
+  let latestTransDate = ''
+  let latestOdoReading = 0
   store.allReadings.forEach((reading) => {
-    if (reading.transaction_date >= lastTransDate && reading.transaction_date < newTransDate) {
-      lastTransDate = reading.transaction_date
-      lastOdoReading = reading.odometer_reading
+    if (reading.transaction_date >= latestTransDate && reading.transaction_date < newEntryTransDate) {
+      latestTransDate = reading.transaction_date
+      latestOdoReading = reading.odometer_reading
     }
   })
 
-  // Ensure that the entered odometer reading is greater than the last odometer reading
-  if (newOdoReading <= lastOdoReading) {
+  // Ensure that the entered odometer reading is greater than the latest odometer reading
+  if (newEntryOdoReading <= latestOdoReading) {
     util.displaySuccessFail(`${pkgName}.paramsAreValid`, 'Inconsistent data. Did you drive in reverse?', false, '')
     util.timeoutMessage()
     util.resetForm()
     return false
-  } else {
+  }
+
+  if (newEntryTransDate <= lastTransDate) {
+    // Create reverse array for backwards checking
+    const reverseReadings = store.allReadings.slice()
+    reverseReadings.reverse()
+
+    let earliestTransDate = lastTransDate
+    let earliestOdoReading = lastOdoReading
+    reverseReadings.forEach((reading) => {
+      if (reading.transaction_date < earliestTransDate && reading.transaction_date >= newEntryTransDate) {
+        earliestTransDate = reading.transaction_date
+        earliestOdoReading = reading.odometer_reading
+      }
+    })
+
+    // Ensure that the entered odometer reading is less than the earliest odometer reading
+    if (newEntryOdoReading >= earliestOdoReading) {
+      util.displaySuccessFail(`${pkgName}.paramsAreValid`, 'Inconsistent data. New entry odometer reading greater than it should be.', false, '')
+      util.timeoutMessage()
+      util.resetForm()
+      return false
+    } else {
+      return true
+    }
+  } else { // newEntryTransDate > lastTransDate
     return true
   }
 }
